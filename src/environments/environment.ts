deb74ac7cd2939a0fa23f81601b6413bd405942f
archie <emmalyn.bustillo@evsu.edu.ph>
2021-05-29 16:18:53 +0800

    Activity Five

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9T_gnAa_ZfQ-5q4Aojkok3XK3VFpuBJU",
+    authDomain: "emmalynactivity-af74c.firebaseapp.com",
+    databaseURL: "emmalynactivity-af74c-default-rtdb.firebaseio.com",
+    projectId: "emmalynactivity-af74c",
+    storageBucket: "emmalynactivity-af74c.appspot.com",
+    messagingSenderId: "99302299218",
+    appId: "1:99302299218:web:d860e0f2e31ba14721718b",
+    measurementId: "G-FNZCCZWGVP"
+    }
 };
 
 /*
